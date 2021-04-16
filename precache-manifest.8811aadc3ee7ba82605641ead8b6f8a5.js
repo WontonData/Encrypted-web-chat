327db863d8a15ae7f97aded488b0aa799ba8139b
water127 <1561513500@qq.com>
2021-04-17 01:22:09 +0800

    Updates

---
+++ precache-manifest.8811aadc3ee7ba82605641ead8b6f8a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18c2a1aee0d3b02a2f261729f693f24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76854df2ea99c0ca83d0",
+    "url": "/static/css/2.95bc0622.chunk.css"
+  },
+  {
+    "revision": "3f048bf2fd1a7b2f7529",
+    "url": "/static/css/main.e41d9c17.chunk.css"
+  },
+  {
+    "revision": "76854df2ea99c0ca83d0",
+    "url": "/static/js/2.6e78be64.chunk.js"
+  },
+  {
+    "revision": "25e5990e64356ccecee0628ceb00187f",
+    "url": "/static/js/2.6e78be64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f048bf2fd1a7b2f7529",
+    "url": "/static/js/main.662889b4.chunk.js"
+  },
+  {
+    "revision": "abda4ea45792181fa420",
+    "url": "/static/js/runtime-main.fce70f5c.js"
+  }
+]);
